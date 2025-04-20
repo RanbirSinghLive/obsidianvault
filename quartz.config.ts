283988a436b31d4d4869ad6981b995e81ccbe2be
Ranbir Singh <ranbirsingh@ranbirs-m1.home>
2025-04-19 21:49:58 -0400

    Update site configuration and content

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ranbir.co",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
